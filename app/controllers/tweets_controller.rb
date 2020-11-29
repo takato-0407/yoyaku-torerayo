a766110049ef4333aa39fb60a5250ce0291c3a06
takato-0407 <70625268+takato-0407@users.noreply.github.com>
2020-11-29 20:58:34 +0900

    ツイート一覧機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,9 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
+
   def index
-    @tweets = Item.all.order(created_at: :DESC)
+    @tweets = Tweet.all.order(created_at: :DESC)
   end
   def new
     @tweet = Tweet.new
@@ -17,9 +19,32 @@ class TweetsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweet_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to root_path
+  end
+
   private
 
   def tweet_params
     params.require(:tweet).permit(:name, :text).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @tweet = Tweet.find(params[:id])
   end
 end
